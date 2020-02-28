03844110b9d99b795c0ce6633612df33239f9f6d
dog-ears <info@dog-ears.net>
2020-02-29 06:19:27 +0900

    setup firebase function nuxtServer

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
+console.info('------------------------------')
+console.info('functions/index.js start')
+console.info('------------------------------')
+
+// dotEnvの読み込み
+require('dotenv').config()
+
+console.log('process.env.NODE_ENV is ' + process.env.NODE_ENV)
+
+// firebase-functionsの読み込み
 const functions = require('firebase-functions')
+
+// Nuxt およびコンフィグの読み込み
+const { Nuxt } = require('nuxt')
+const config = require('./nuxt.config.js')
+
+// コンフィグ内、@nuxtjs/eslint-moduleは、firebaseにデプロイするとnotFoundになるため削除
+config.buildModules = config.buildModules.filter(
+  (n) => n !== '@nuxtjs/eslint-module'
+)
+
+// 強制的にproductionモードにする
+config.dev = false
+
+// nuxt生成
+const nuxt = new Nuxt(config)
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.nuxtServer = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
+exports.nuxtServer = functions.https.onRequest(async (request, response) => {
+  const result = await nuxt.renderRoute(request.url)
+  response.send(result.html)
 })
